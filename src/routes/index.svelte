02a8b2bf2232faa4290e4b3c348f844afda76d0d
Sarah Norris <sarah@sekai.co.uk>
2022-07-30 18:16:36 +0100

    chore: install sveltekit & postcss; add initial content

---
+++ src/routes/index.svelte
@@ -0,0 +1,98 @@
+<script>
+    // contact form js
+
+    // import 'ress';
+</script>
+
+<main>
+    <header>
+        <div><a href="tel:07973788953">07973 788953</a></div>
+        <ul class="social">
+            <li>Behance</li>
+            <li>Pinterst</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+        </ul>
+    </header>
+
+    <h1>ZOOT Design</h1>
+    <h2>New Website Coming Soon</h2>
+    <p>Are you lost in space? Burning in the dark alone, perhaps we can help those flames flourish into a burning light to capture the attentions of your designated audience? If you would like to discuss a way to control those flames get in touch by filling out the form below or stay in touch by following us...</p>
+
+    <div>
+        <h3>Follow</h3>
+        <ul class="social">
+            <li>Behance</li>
+            <li>Pinterst</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+        </ul>
+    </div>
+
+    <h2>Get in Touch</h2>
+    <p>If you would like us to quote on a particular project or if you would like to discuss the potential of a project with us or perhaps you have a question on one of our services if so, fill out the form below and we will get back to you promptly with a reply and or an answer...</p>
+
+    <!-- Form -->
+
+    <h2>What We Do</h2>
+    <p>Read more about what we do perhaps we are exactly what you are looking for...</p>
+
+    <div class="whatwedo">
+        <div>
+            <h3>Bespoke Graphic Design</h3>
+            <p>Anyone can buy images and move them around on a screen, that's easy.</p>
+            <p>But to have the creative know-how to implement your vision with pixel perfect clarity and deliver it to your target audience takes experience and skill and we have oodles of it at ZOOT.</p>
+        </div>
+        <div>
+            <h3>Website and Online Design</h3>
+            <p>A web presence is essential, it's your shop window, it's an invitation and a call to action.</p>
+            <p>A website is the hub of any business in the 21st century and should be designed to give an ultimate user experience. We can help plan and develop that experience to a professional standard.</p>
+        </div>
+        <div>
+            <h3>Device Apps</h3>
+            <p>Most phones run apps, cool apps, informative apps, quirky apps, convenience apps.</p>
+            <p>Apps are pocket sized packages of convenience that fit snuggly into our lives aiding us with day to day information, if you have an app idea, let us help you develop it.</p>
+        </div>
+    </div>
+
+    <footer>
+        <div>
+            <span>Zoot logo</span>
+            <div>
+                Copyright Notice.<br>
+                &copy; ZOOT Design. All rights reserved.<br>
+                Material published by ZOOT Design on these web pages is copyright ZOOT Design and may not be reproduced without permission.
+            </div>
+        </div>
+        <div>
+            <span>Footer graphics</span>
+        </div>
+    </footer>
+</main>
+
+<style global>
+    @import 'ress';
+
+    main {
+        text-align: center;
+    }
+    header {
+        background: #000;
+        color: #fff;
+    }
+    header, footer, .whatwedo {
+        display: flex;
+    }
+    header, footer {
+        justify-content: space-between;
+        align-items: center;
+        text-align: left;
+    }
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+    ul li {
+        display: inline-block;
+    }
+</style>
